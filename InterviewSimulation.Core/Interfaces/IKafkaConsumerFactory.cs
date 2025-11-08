7428d29671770c3e2ea9bad469ef892ef75100f0
NGUYEN TIEN HUNG <hungnthe176686@fpt.edu.vn>
2025-11-09 01:37:16 +0700

    Delete InterviewSimulation.Core directory

--- InterviewSimulation.Core/Interfaces/IKafkaConsumerFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using InterviewSimulation.Core.Interfaces;
-
-namespace InterviewSimulation.Infrastructure.Kafka
-{
-    public interface IKafkaConsumerFactory<T> where T : class
-    {
-        IKafkaConsumerRepository<T> CreateConsumer(string sourceServiceName);
-    }
-}
